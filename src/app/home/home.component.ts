64bc5f2c16fb2d16acf6604dc0a29a9aac8767b1
Molly Stone <molly.stone89@gmail.com>
2019-07-18 16:14:18 -0400

    Add home and list routes and components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+    clickCounter: number = 0;
+    name: string = '';
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    countClick() {
+        this.clickCounter++;
+    }
+
+    setClasses() {
+        let classes = {
+            active: this.clickCounter > 4,
+            notActive: this.clickCounter <= 4
+        }
+        return classes;
+    }
+
+}
